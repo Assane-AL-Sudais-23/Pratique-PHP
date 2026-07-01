344425878542cf814c2d0a07f5369c5a2c6f453a
Assane AL Sudais <assanesidyalsudais2004@gmail.com>
2026-07-01 01:50:15 +0000

    initialisation du projet

---
+++ imperatif.php
@@ -0,0 +1,19 @@
+<§<?php
+    // 1 Initialiser un tableau de categorie
+
+    // 2 Afficher les categorie sans produits
+
+    // 3 enrigistrer une nouvelle categorie(code et nom unique et obligatoire, Produit vide);
+
+    // 4 Ajouter un produit a une categorie
+        /*
+            a - recherche categorie par code
+            b - saisir infons produits si categorie trouve
+            nb : lors saisie reference unique et obligatoire
+        */
+
+    // 5 - Ajout d'une categorie en lui affectant des produits
+        // tant user reponds oui produits sera ajouter
+
+
+?>
