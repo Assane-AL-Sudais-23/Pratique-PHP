43abdc0fcf7ce19f2894207717f151a80cdc024d
Assane AL Sudais <assanesidyalsudais2004@gmail.com>
2026-07-01 02:32:00 +0000

    Initialisation d'un tableau de categorie*

--- imperatif.php
+++ imperatif.php
@@ -1,5 +1,26 @@
 <§<?php
     // 1 Initialiser un tableau de categorie
+    $categories = [];
+    
+    $categories[] = [
+        0 => [
+            "nom" => "categorieA",
+            "code" => "1212",
+            "produits" => [
+                0 => ["nom" => "Lait", "reference" => "00123", "quantite" => "3", "prix" => "1500"],
+                1 => ["nom" => "Sucre", "reference" => "00124", "quantite" => "7", "prix" => "300"],
+                2 => ["nom" => "Sel", "reference" => "00125", "quantite" => "5", "prix" => "250"],
+            ]
+        ],
+        1 => [
+            "nom" => "categorieB",
+            "code" => "2323",
+            "produits" => [
+                0 => ["nom" => "Pain", "reference" => "00127", "quantite" => "10", "prix" => "150"],
+                1 => ["nom" => "Riz", "reference" => "00130", "quantite" => "6", "prix" => "450"]
+            ]
+        ]
+    ]
 
     // 2 Afficher les categorie sans produits
 
